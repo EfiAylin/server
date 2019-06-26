b0d5bc9f9dcd9bdff28d82d40367c902fbf2b767
aylineaylin <efiaylin1@gmail.com>
2019-06-26 12:17:46 +0300

    setting up to push to Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ h1: 'there' });
+    res.send({ h1: 'Hello World' });
 });
 
 const PORT = process.env.PORT || 5000;
